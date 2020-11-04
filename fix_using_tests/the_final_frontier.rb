2f2b7d04718927311b64e0f33642169c3552b60b
Charlie Knight <williamchknight@gmail.com>
2020-11-04 17:59:45 +0000

    Done.

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,6 +1,7 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
-def generate_star_date
+def date
   (rand(100000) + 400000) / 10.0
 end
 
@@ -11,5 +12,4 @@ end
 
 def engage
   puts state_log(date)
-  date = generate_star_date
 end
